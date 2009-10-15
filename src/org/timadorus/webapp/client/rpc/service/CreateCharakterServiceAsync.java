af504eb458f3790edfd24bff1c206013fd7590d7
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2009-10-15 11:32:52 +0000

    fix object/method names in CreateCharacter*
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk/TimadorusWebApp@516 aa37ab44-2659-0410-8a83-c73d69c9b566

--- src/org/timadorus/webapp/client/rpc/service/CreateCharakterServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package org.timadorus.webapp.client.rpc.service;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface CreateCharakterServiceAsync {
-	void createCharakter(String input, AsyncCallback<String> callback);
-}
