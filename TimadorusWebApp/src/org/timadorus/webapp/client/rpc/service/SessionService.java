2b2b23926b8df8a1889df4f08e51601428a6c4ae
Michael Pawlik <Michael.Pawlik@informatik.haw-hamburg.de>
2009-10-26 16:21:20 +0000

    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/branches/without_usability_test@530 aa37ab44-2659-0410-8a83-c73d69c9b566

---
+++ TimadorusWebApp/src/org/timadorus/webapp/client/rpc/service/SessionService.java
@@ -0,0 +1,10 @@
+package org.timadorus.webapp.client.rpc.service;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import org.timadorus.webapp.client.SessionId;
+
+@RemoteServiceRelativePath("session")
+public interface SessionService extends RemoteService{
+	SessionId session(SessionId sessionId);
+}
