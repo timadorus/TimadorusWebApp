b9b9e0bb7522a4c068849401d97cfd322cbf9679
Ali Kilic <kilic_a@informatik.haw-hamburg.de>
2009-11-09 11:32:21 +0000

    + states of Browsing history
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/branches/with_usability_refactored@646 aa37ab44-2659-0410-8a83-c73d69c9b566

---
+++ src/org/timadorus/webapp/client/HistorySates.java
@@ -0,0 +1,13 @@
+package org.timadorus.webapp.client;
+
+//States of the Browsing history
+public interface HistorySates {
+  public static final String LOGIN_STATE = "login";
+
+  public static final String WELCOME_STATE = "welcome";
+
+  public static final String CREATE_STATE = "create";
+
+  public static final String REGISTER_STATE = "register";
+  
+}
