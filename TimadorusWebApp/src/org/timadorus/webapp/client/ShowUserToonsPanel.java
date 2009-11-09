83f58d80652671fd23e80cdb339e605bbdfd7f65
Michael Pawlik <Michael.Pawlik@informatik.haw-hamburg.de>
2009-11-09 12:44:47 +0000

    ToonPanel
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/branches/without_usability_recoded@655 aa37ab44-2659-0410-8a83-c73d69c9b566

--- TimadorusWebApp/src/org/timadorus/webapp/client/ShowUserToonsPanel.java
+++ TimadorusWebApp/src/org/timadorus/webapp/client/ShowUserToonsPanel.java
@@ -1,12 +1,11 @@
 package org.timadorus.webapp.client;
 
-import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.TextBox;
 
-public class ShowUserToonsPanel extends FormPanel {
+public class ShowUserToonsPanel extends VerticalPanel {
 	private final TimadorusWebApp appReference;
 	
 	public ShowUserToonsPanel() throws Exception {
@@ -21,10 +20,21 @@ public class ShowUserToonsPanel extends FormPanel {
 	
 	private void initialize() {
 		if(this.appReference.getUserLoggedIn()) {
-			final Grid showUserProfileGrid					= new Grid(5, 2);
+			int rows = 1 / 3;
+			final Grid showUserToons = new Grid(5, 3);
 			
-			this.add(new HTML("<h2>My Profile</h2>"));
-			this.add(showUserProfileGrid);
+			//for (Toon toonOfUser : ToonSet)
+			VerticalPanel tempPanel = new VerticalPanel();
+			tempPanel.add(new HTML("<h3>"+toonOfUser.getToonName()+"</h3>"));
+			tempPanel.add(new HTML("Gender: "+toonOfUser.getGender()));
+			tempPanel.add(new HTML("Race:"+toonOfUser.getRace()));
+			tempPanel.add(new HTML("Fraction:"+toonOfUser.getFraction()));
+			tempPanel.add(new HTML("Proffesion:"+toonOfUser.getProffesion()));
+			
+			showUserToons.setWidget(1, 1, new Label(""));
+			
+			this.add(new HTML("<h2>My Toons</h2>"));
+			this.add(showUserToons);
 		}
 	}
 }
