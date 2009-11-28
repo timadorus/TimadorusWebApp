c74c62260b0bfd4f85fb25aa82076e9c15eeeb50
Ali Kilic <kilic_a@informatik.haw-hamburg.de>
2009-11-28 17:39:00 +0000

    prepare some sample Races and Classes
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/branches/with_usability_refactored@745 aa37ab44-2659-0410-8a83-c73d69c9b566

--- src/org/timadorus/webapp/client/character/Class.java
+++ src/org/timadorus/webapp/client/character/Class.java
@@ -4,18 +4,25 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Class implements Serializable{
-  
+public class Class implements Serializable {
+
   String name;
-  
+
   String description;
-  
+
   private static final long serialVersionUID = 1L;
-  
+
   public Class() {
-    
+
   }
-  
+
+  public Class(String name, String description) {
+
+    this.name = name;
+    this.description = description;
+
+  }
+
   public String getName() {
     return name;
   }
@@ -31,7 +38,32 @@ public class Class implements Serializable{
   public void setDescription(String description) {
     this.description = description;
   }
-  
-  
-  
+
+  public static List<Class> getSampleClasses() {
+
+    Class c1 = new Class(
+                         "Capitalist class",
+                         "Top-level executives, high-rung politicians, heirs. Ivy League education common. Source: http://en.wikipedia.org/wiki/Social_class");
+
+    Class c2 = new Class(
+                         "Upper middle class",
+                         "Highly educated (often with graduate degrees), most commonly salaried, professionals and middle management with large work autonomy. Source: http://en.wikipedia.org/wiki/Social_class");
+
+    Class c3 = new Class(
+                         "Working class",
+                         "Clerical and most blue collar workers whose work is highly routinized. Standard of living varies depending on number of income earners, but is commonly just adequate. High school education. Source: http://en.wikipedia.org/wiki/Social_class");
+
+    Class c4 = new Class(
+                         "Underclass",
+                         "Those with limited or no participation in the labor force. Reliant on government transfers. Some high school education. Source: http://en.wikipedia.org/wiki/Social_class");
+
+    ArrayList<Class> clist = new ArrayList<Class>();
+    clist.add(c1);
+    clist.add(c2);
+    clist.add(c3);
+    clist.add(c4);
+
+    return clist;
+  }
+
 }
--- src/org/timadorus/webapp/client/character/Race.java
+++ src/org/timadorus/webapp/client/character/Race.java
@@ -13,6 +13,8 @@ import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
+
+
 /**
  * @author maddin
  * 
@@ -40,6 +42,14 @@ public class Race implements Serializable {
     super();
   }
 
+  public Race(Long raceID, String name, String description) {
+
+    this.raceID = raceID;
+    this.name = name;
+    this.description = description;
+
+  }
+  
   public Long getRaceID() {
     return raceID;
   }
@@ -74,6 +84,36 @@ public class Race implements Serializable {
   
   public void addClass(Class newClass){
     availableClasses.add(newClass);    
+  }
+  
+  public static List<Race> getSampleRaces() {
+
+    Race r1 = new Race(
+                       new Long(001),
+                       "Race-Almas",
+                       "The Almas, Mongolian for \"wild man,\" is a purported hominid cryptozoological species reputed to inhabit the Caucasus and Pamir Mountains of central Asia, and the Altai Mountains of southern Mongolia. Source: http://en.wikipedia.org/wiki/Almas_%28cryptozoology%29");
+    Race r2 = new Race(
+                       new Long(002),
+                       "Race-Amomongo",
+                       "The Amomongo is a creature of Philippine mythology described as hairy, man-sized and ape-like with long nails. Source: http://en.wikipedia.org/wiki/Amomongo");
+
+    Race r3 = new Race(
+                       new Long(003),
+                       "Race-Chuchunya",
+                       "Chuchunya is a hominid cryptid rumoured to exist in Siberia, Russia. It has been described as six to seven feet tall and covered with dark hair. Source: http://en.wikipedia.org/wiki/Tjutjuna");
+
+    Race r4 = new Race(
+                       new Long(004),
+                       "Race-Yeti",
+                       "The Yeti or Abominable Snowman is a creature and an ape-like cryptid said to inhabit the Himalayan region of Nepal and Tibet. The names Yeti and Meh-Teh are commonly used by the people indigenous to the region,[1] and are part of their history and mythology. Stories of the Yeti first emerged as a facet of Western popular culture in the 19th century. Source: http://en.wikipedia.org/wiki/Yeti");
+
+    ArrayList<Race> rlist=new ArrayList<Race>();
+    rlist.add(r1);
+    rlist.add(r2);
+    rlist.add(r3);
+    rlist.add(r4);
+    
+    return rlist;
   }
 
 }
