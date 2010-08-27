6e805bf4dfa9d942cd26cf87efde6f4121d43129
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2010-08-27 09:48:15 +0000

    remove old WepApp code
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@965 aa37ab44-2659-0410-8a83-c73d69c9b566

--- TimadorusWebApp/src/org/timadorus/webapp/client/rpc/service/CreateCharacterService.java
+++
@@ -1,9 +0,0 @@
-package org.timadorus.webapp.client.rpc.service;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-@RemoteServiceRelativePath("greet")
-public interface CreateCharacterService extends RemoteService {
-	Character createCharacter(String name);
-}
--- TimadorusWebApp/src/org/timadorus/webapp/client/rpc/service/CreateCharacterServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package org.timadorus.webapp.client.rpc.service;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface CreateCharacterServiceAsync {
-	void createCharacter(String input, AsyncCallback<Character> callback);
-}
