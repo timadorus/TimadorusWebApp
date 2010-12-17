a7014e1c4389da66ebd247dcfaf4cda7e42c74c5
Thorben Woggan <torben.woggan@haw-hamburg.de>
2010-12-17 21:06:10 +0000

    deleting a character now requires to type in the password
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@1329 aa37ab44-2659-0410-8a83-c73d69c9b566

--- src/org/timadorus/webapp/client/character/ShowCharacterlistPanel.java
+++ src/org/timadorus/webapp/client/character/ShowCharacterlistPanel.java
@@ -10,13 +10,18 @@ import org.timadorus.webapp.client.rpc.service.CharacterServiceAsync;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -30,14 +35,13 @@ public final class ShowCharacterlistPanel extends FormPanel {
 
   VerticalPanel panel       = new VerticalPanel();
   HTML headline             = new HTML("<h1>Liste der registrierten Charaktere</h1>");
+  PasswordTextBox passbox   = new PasswordTextBox();
 
   private ShowCharacterlistPanel(TimadorusWebApp entryIn, User userIn) {
     super();
 
     this.entry = entryIn;
     this.user  = userIn;
-    
-    
     
     getCharactersFromServer();
     
@@ -86,10 +90,21 @@ public final class ShowCharacterlistPanel extends FormPanel {
       for (final Character character : result) {
         final Button delete = new Button("Löschen");
         final Button details = new Button("Details");
+        final Button confirm = new Button("Löschen");
+        
         class MyHandler implements ClickHandler {
           public void onClick(ClickEvent event) {
             if (event.getSource().equals(delete)) {
-              deleteCharacter(character);
+              grid.clear();
+              panel.clear();
+              final int rows = 3;
+              grid = new Grid(rows, 1);
+              headline = new HTML("<h1>Charakter löschen</h1>");
+              grid.setWidget(0, 0, new Label("Sind Sie sich sicher, dass Sie diesen Charakter löschen wollen?"));
+              grid.setWidget(1, 0, passbox);
+              grid.setWidget(2, 0, confirm);
+              panel.add(headline);
+              panel.add(grid);
             } else if (event.getSource().equals(details)) {
               grid.clear();
               panel.clear();
@@ -100,9 +115,46 @@ public final class ShowCharacterlistPanel extends FormPanel {
             }      
           }      
         }
+        
+        class ConfirmHandler implements ClickHandler, KeyUpHandler {
+          /**
+           * Wird ausgelöst, wenn Button gedrückt wurde
+           */
+          public void onClick(ClickEvent event) {
+            System.out.println("Löschung bestätigen Button geklickt");
+            handleEvent();
+          }
+
+          /**
+           * Prüft ob "Enter" gedrückt wurde
+           */
+          public void onKeyUp(KeyUpEvent event) {
+            if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+              handleEvent();
+            }
+          }
+
+          private void handleEvent() {
+            System.out.println("handle Event");
+            if (passbox.getText().equals(user.getPassword())) {
+              System.out.println("Deleting " + character.getName());
+              deleteCharacter(character);
+              showDialogBox("Information", "Ihr Charakter wurde erfolgreich gelöscht!");
+            } else {
+              passbox.setText("");
+              showDialogBox("Fehlermeldung", "Passwort falsch! Versuchen Sie es erneut!");
+            }
+          }
+        }
+        
         MyHandler handler = new MyHandler();
         delete.addClickHandler(handler);
         details.addClickHandler(handler);
+        
+        ConfirmHandler handler2 = new ConfirmHandler();
+        confirm.addClickHandler(handler2);
+        confirm.addKeyUpHandler(handler2);
+        
         grid.setWidget(i, 0, ShowCharacterPanel.getShowShortCharacterPanel(entry, user, character));
         grid.setWidget(i, 1, details);
         grid.setWidget(i, 2, delete);
@@ -164,5 +216,34 @@ public final class ShowCharacterlistPanel extends FormPanel {
 
   public void setUser(User userIn) {
     this.user = userIn;
+  }
+  
+  public void showDialogBox(String title, String message) {
+    // Create the popup dialog box
+    final DialogBox dialogBox = new DialogBox();
+
+    dialogBox.setText(title);
+    dialogBox.setAnimationEnabled(true);
+    final Button closeButton = new Button("Close");
+
+    // We can set the id of a widget by accessing its Element
+    closeButton.getElement().setId("closeButton");
+
+    VerticalPanel dialogVPanel = new VerticalPanel();
+    dialogVPanel.addStyleName("dialogVPanel");
+
+    dialogVPanel.add(new HTML((new StringBuffer().append("<b>").append(message).append("</b>")).toString()));
+    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+    dialogVPanel.add(closeButton);
+    dialogBox.setWidget(dialogVPanel);
+
+    dialogBox.center();
+
+    // Add a handler to close the DialogBox
+    closeButton.addClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        dialogBox.hide();
+      }
+    });
   }
 }
