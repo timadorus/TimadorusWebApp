6e746d49ac4c81645874e48403e9992e5b15aebf
Thorben Woggan <torben.woggan@haw-hamburg.de>
2010-12-19 15:13:11 +0000

    first test now working and passing
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@1338 aa37ab44-2659-0410-8a83-c73d69c9b566

--- src/org/timadorus/webapp/tests/selenium/RegisterUserTest.java
+++ src/org/timadorus/webapp/tests/selenium/RegisterUserTest.java
@@ -1,8 +1,9 @@
 package org.timadorus.webapp.tests.selenium;
 
-import com.thoughtworks.selenium.*;
-
-public class RegisterUserTest extends SeleneseTestCase {
+public class RegisterUserTest extends WebTestCase {
+  
+  final int sleepTime = 20000;
+  
   @Override
   public void setUp() throws Exception {
     setUp("http://127.0.0.1:8888/", "*firefox");
@@ -10,25 +11,27 @@ public class RegisterUserTest extends SeleneseTestCase {
 
   public void testRegisterUser() throws Exception {
     selenium.open("/TimadorusWebApp.html");
-    selenium.click("link=Account registrieren");
-    selenium.type("//input[@type='text']", "Test");
-    selenium.type("//div[@id='content']/form/table/tbody/tr[2]/td[2]/input", "1");
-    selenium.type("//div[@id='content']/form/table/tbody/tr[3]/td[2]/input", "31.10.1988");
-    selenium.type("//div[@id='content']/form/table/tbody/tr[4]/td[2]/input", "test1@home.de");
-    selenium.type("//div[@id='content']/form/table/tbody/tr[5]/td[2]/input", "test1@home.de");
-    selenium.type("//div[@id='content']/form/table/tbody/tr[6]/td[2]/input", "test1");
-    selenium.type("//input[@type='password']", "geheim");
-    selenium.type("//div[@id='content']/form/table/tbody/tr[8]/td[2]/input", "geheim");
-    selenium.click("//button[@type='button']");
-    selenium.click("link=Einloggen");
-    selenium.type("//input[@type='text']", "test1");
-    selenium.type("//input[@type='password']", "geheim");
-    selenium.click("//button[@type='button']");
+    click("link=Account registrieren");
+    type("//input[@type='text']", "Test");
+    type("//div[@id='content']/form/table/tbody/tr[2]/td[2]/input", "1");
+    type("//div[@id='content']/form/table/tbody/tr[3]/td[2]/input", "31.10.1988");
+    type("//div[@id='content']/form/table/tbody/tr[4]/td[2]/input", "test1@home.de");
+    type("//div[@id='content']/form/table/tbody/tr[5]/td[2]/input", "test1@home.de");
+    type("//div[@id='content']/form/table/tbody/tr[6]/td[2]/input", "test1");
+    type("//input[@type='password']", "geheim");
+    type("//div[@id='content']/form/table/tbody/tr[8]/td[2]/input", "geheim");    
+    click("//button[@type='button']");
+    // creation of table "users"    
+    Thread.sleep(sleepTime);
+    click("link=Einloggen");
+    type("//input[@type='text']", "test1");
+    type("//input[@type='password']", "geheim");
+    click("//button[@type='button']");
     verifyTrue(selenium.isTextPresent("Du bist als test1 angemeldet"));
     verifyTrue(selenium.isTextPresent("Liste der Charaktere"));
     verifyTrue(selenium.isTextPresent("Profil bearbeiten"));
     verifyTrue(selenium.isTextPresent("Ausloggen"));
-    selenium.click("link=Ausloggen");
+    click("link=Ausloggen");
     verifyTrue(selenium.isTextPresent("Sie haben sich ausgeloggt."));
   }
 }
