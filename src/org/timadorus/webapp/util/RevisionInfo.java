0310b5989242c52b6a409b9a6eb92bbe6aa7cd11
Domenik Boukherbata <domenik.boukherbata@haw-hamburg.de>
2011-04-11 11:37:14 +0000

    Created class for Revision number. Made some preparations for the Database Migration.
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@1636 aa37ab44-2659-0410-8a83-c73d69c9b566

---
+++ src/org/timadorus/webapp/util/RevisionInfo.java
@@ -0,0 +1,9 @@
+
+            package org.timadorus.webapp.util;
+              public final class RevisionInfo {
+              private RevisionInfo() {
+              }
+              public static final String REVISION_NUMBER = "-1";
+              public static final String TIME_STAMP = "Mon, 11-April-2011 13:35:06 CEST";
+            }
+        
