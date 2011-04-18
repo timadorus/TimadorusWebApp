f34e139e6402927c1ed7d0d89e4854d1b795d490
Domenik Boukherbata <domenik.boukherbata@haw-hamburg.de>
2011-04-18 08:15:05 +0000

    Added ivy dependency for the Winstone Server
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@1656 aa37ab44-2659-0410-8a83-c73d69c9b566

--- src/org/timadorus/webapp/util/RevisionInfo.java
+++ src/org/timadorus/webapp/util/RevisionInfo.java
@@ -3,7 +3,7 @@
               public final class RevisionInfo {
               private RevisionInfo() {
               }
-              public static final String REVISION_NUMBER = "1636";
-              public static final String TIME_STAMP = "Sun, 17-April-2011 02:03:50 CEST";
+              public static final String REVISION_NUMBER = "1648";
+              public static final String TIME_STAMP = "Mon, 18-April-2011 10:02:09 CEST";
             }
         
