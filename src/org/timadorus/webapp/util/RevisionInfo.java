19ae9798fb354fa86fa8083e340324cc4f4e5ebd
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2011-04-19 06:09:01 +0000

    removed (it is a created element. It will lead to continuous svn conflicts if left in svn)
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@1663 aa37ab44-2659-0410-8a83-c73d69c9b566

--- src/org/timadorus/webapp/util/RevisionInfo.java
+++
@@ -1,9 +0,0 @@
-
-            package org.timadorus.webapp.util;
-              public final class RevisionInfo {
-              private RevisionInfo() {
-              }
-              public static final String REVISION_NUMBER = "1648";
-              public static final String TIME_STAMP = "Mon, 18-April-2011 10:02:09 CEST";
-            }
-        
