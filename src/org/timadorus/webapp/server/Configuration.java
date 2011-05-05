dc10a4a90a5323186cdf03396d68a5c177a99f88
Domenik Boukherbata <domenik.boukherbata@haw-hamburg.de>
2011-05-05 11:14:55 +0000

    Added Configuration class and the needed ivy dependencies
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@1705 aa37ab44-2659-0410-8a83-c73d69c9b566

---
+++ src/org/timadorus/webapp/server/Configuration.java
@@ -0,0 +1,67 @@
+package org.timadorus.webapp.server;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+
+/**
+ * Configuration class for every a attribute of the WebApp
+ *
+ */
+public final class Configuration {
+  
+  public static final PersistenceManagerFactory PMF = JDOHelper.getPersistenceManagerFactory("transactions-optional");
+
+  private static String key, value;
+
+  private Configuration() {
+  }
+ 
+  public String getValue() {
+    return this.value;
+  }
+  
+  public void setValue(String value) {
+    this.value = value;
+  }
+  
+  /**
+   * This method sets a configuration.
+   * 
+   */
+  public static void setConfiguration(String key1, String value1) {  
+    PersistenceManager pm = PMF.getPersistenceManager();
+    key = key1;
+    value = value1;
+    
+    Query query = pm.newQuery(Configuration.class, "key == " + key);
+    query.setUnique(true);
+    Configuration configuration = (Configuration) query.execute(query);
+    
+    if (configuration == null) {
+      pm.makePersistent(Configuration.class);
+    } else {
+      configuration.setValue(value1);
+      pm.makePersistent(configuration);
+    }
+
+    pm.close();
+    System.out.println("Datastore: Configuration hinzugefügt mit Key, Value: " + key + ", " + value);
+  }
+
+  /**
+   * This method gets a configuration .
+   * 
+   * @param The key for the mapped value
+   * @return The value
+   */
+  public static String getConfiguration(String key) {
+    PersistenceManager pm = PMF.getPersistenceManager();
+    Query query = pm.newQuery(Configuration.class, "key == " + key);
+    query.setUnique(true);
+    Configuration configuration = (Configuration) query.execute(query);
+    return configuration.getValue();
+  }
+  
+}
