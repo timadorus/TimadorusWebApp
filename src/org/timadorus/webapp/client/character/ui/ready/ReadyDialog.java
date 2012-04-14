1d5f4cd88e916da621e6a4f05201af93341a7ba5
Sebastian Witt <sebastian.witt@haw-hamburg.de>
2012-04-14 12:12:55 +0000

    === missed classes of previous changelist === Refactored CreateCharacterPanel for mvp pattern. Adjust corresponding Classes
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@2227 aa37ab44-2659-0410-8a83-c73d69c9b566

--- src/org/timadorus/webapp/client/character/ui/ready/ReadyDialog.java
+++ src/org/timadorus/webapp/client/character/ui/ready/ReadyDialog.java
@@ -2,18 +2,11 @@ package org.timadorus.webapp.client.character.ui.ready;
 
 import org.timadorus.webapp.client.TimadorusWebApp;
 import org.timadorus.webapp.client.character.Character;
+import org.timadorus.webapp.client.character.ui.DefaultDisplay;
+import org.timadorus.webapp.client.character.ui.DefaultWidget;
 
-import com.google.gwt.user.client.ui.FormPanel;
-
-public class ReadyDialog {
-  public interface Display {
-
-    /**
-     * Returns the GWI FormPanal in which the widgets offer his controls.
-     * 
-     * @return {@link FormPanel}
-     */
-    public FormPanel getFormPanel();
+public class ReadyDialog extends DefaultWidget<ReadyDialog.Display> {
+  public interface Display extends DefaultDisplay {
 
     public void setContragulationMessage(String msg);
 
@@ -26,29 +19,17 @@ public class ReadyDialog {
   private final String informationMsg = "<h1>Charakter erstellt</h1><p>Herzlichen Glückwunsch"
       + ". Sie können nun den Client starten und ihren neuen Charakter ausprobieren!";
 
-  private TimadorusWebApp entry;
-
-  private ReadyDialog.Display display;
-
-  public ReadyDialog(TimadorusWebApp entry, ReadyDialog.Display display) {
-    this.entry = entry;
-    this.display = display;
-    this.display.setContragulationMessage(congratulationMsg);
-    this.display.setInformationMessage(informationMsg);
+  public ReadyDialog(ReadyDialog.Display display, TimadorusWebApp entry) {
+    super(display, entry);
+    
+    this.getDisplay().setContragulationMessage(congratulationMsg);
+    this.getDisplay().setInformationMessage(informationMsg);
   }
 
   public static ReadyDialog getReadyDialog(TimadorusWebApp entry, Character characterIn) {
     ReadyDialog.Display display = (Display) new CharacterReadyWidget(characterIn);
-    ReadyDialog dialog = new ReadyDialog(entry, display);
+    ReadyDialog dialog = new ReadyDialog(display, entry);
     return dialog;
-  }
-
-  public FormPanel getFormPanel() {
-    return display.getFormPanel();
-  }
-
-  public TimadorusWebApp getEntry() {
-    return entry;
   }
 
 }
