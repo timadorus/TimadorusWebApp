fd8bfbb5855915c1adaafe02f8568c2874a11fbd
Sebastian Witt <sebastian.witt@haw-hamburg.de>
2012-05-12 22:55:31 +0000

    fourth part of commits... the SVN can't commit all changes at once (Reorganizing eventpackage struckture)
    git-svn-id: svn+ssh://svn.informatik.haw-hamburg.de/srv/svn/timadorus/webapp/trunk@2314 aa37ab44-2659-0410-8a83-c73d69c9b566

--- src/org/timadorus/webapp/client/eventhandling/ShowLoginHandler.java
+++
@@ -1,9 +0,0 @@
-package org.timadorus.webapp.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface ShowLoginHandler extends EventHandler {
-
-  public void showLogin();
-  
-}
