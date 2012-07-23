813bc643ede1fc8c4efae531b7963c578b824469
sage <lutz.behnke@informatik.haw-hambug.de>
2012-07-23 12:05:57 +0200

    clean up ignore, update eclipse classpath to new dependency versions

---
+++ src/org/timadorus/webapp/util/RevisionInfo.java
@@ -0,0 +1,9 @@
+
+            package org.timadorus.webapp.util;
+              public final class RevisionInfo {
+              private RevisionInfo() {
+              }
+              public static final String REVISION_NUMBER = "${revisionVersion}";
+              public static final String TIME_STAMP = "Mon, 23-July-2012 11:51:14 CEST";
+            }
+        
